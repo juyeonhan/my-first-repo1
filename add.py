cf9909c189658a24bbccfd162603e655855fdc1f
juyeonhan <chezhan@gmail.com>
2018-12-08 00:37:03 -0600

    modified1

--- add.py
+++ add.py
@@ -1,4 +1,5 @@
 https://github.com/juyeonhan/my-first-repo1.githttps://github.com/juyeonhan/my-first-repo1.git
-for i in rnage (1, 20+!):
-    
-    print("it works" )
+for i in rnage (1, 20+1):
+    if i%2==0:
+
+        print("it works" )
